cfd19546ae309d5c7e5ce970ed39ae378c532f70
Georgiy Odisharia <math.kraut.cat@gmail.com>
2017-07-26 20:23:22 +0300

    Take-off commit

--- pake/cli_parser.py
+++ pake/cli_parser.py
@@ -1,3 +1,16 @@
+import argparse
+
+
+
 class OptionParser:
     def __init__(self):
-        df
+        self.__parser = argparse.ArgumentParser()
+        subparsers = self.__parser.add_subparsers()
+
+        # create the parser for the "a" command
+        parser_install = subparsers.add_parser('install', help='install help')
+        parser_install.add_argument('bar', type=int, help='bar help')
+
+        # create the parser for the "b" command
+        parser_build = subparsers.add_parser('b', help='b help')
+        parser_build.add_argument('--source', type='store_true', default )
